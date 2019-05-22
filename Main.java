9e0d109da79d1a028eb8a538d4e2a557612c0939
Dandridge Cole <dandridgecole@yahoo.com>
2019-05-22 13:22:09 -0400

    My Sum of Input files -- may Gauss forgive me.

--- Main.java
+++ Main.java
@@ -1,9 +1,27 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] args){
-
+    public void main(){
+        System.out.print("Gauss is turning over in his grave: "+calculateSum(promptForInput()));
     }
+    
+    public Integer promptForInput(){
+        Integer userInput;
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Please enter a positive integer:");
+        userInput = myScanner.nextInt();
+        return userInput;
+    }
+    
+    public Integer calculateSum(Integer input){
+        Integer mySum=0;
+        for(Integer i = 1; i<=input;i++){
+            mySum+=i;
+        }
+        return mySum;
+    }
+    
 }
